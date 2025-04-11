5796682769ed4e8d08a2bd8321a2cb17d9ffe51d
RPDJF <ruipaulo.unify@outlook.fr>
2025-04-12 01:57:00 +0200

    added vpn server

--- all-compose.sh
+++ all-compose.sh
@@ -7,6 +7,9 @@ compose_files=$(find "$path" -name "*-compose-*.yml" | sort -r)
 args="$@"
 
 for file in $compose_files; do
-  docker compose -f "$file" $args
+  if [[ $file == *"wireguard"* ]]; then
+    docker compose --env-file=./conf/wireguard/.env -f "$file" $args
+  else
+    docker compose -f "$file" $args
+  fi
 done
-
