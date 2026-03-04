ba3cf05a606143aa67776f66a03e68d70b051188
RPDJF <ruipaulo.unify@outlook.fr>
2026-03-04 13:19:15 +0100

    update

--- all-compose.sh
+++ all-compose.sh
@@ -8,8 +8,12 @@ args="$@"
 
 for file in $compose_files; do
   if [[ $file == *"wireguard"* ]]; then
+    echo "composing $file"
     docker compose --env-file=./conf/wireguard/.env -f "$file" $args
+  elif [[ $file == *"mcserver"* ]]; then
+      echo "ignoring $file..."
   else
+    echo "composing $file"
     docker compose -f "$file" $args
   fi
 done
